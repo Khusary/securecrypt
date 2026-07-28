cc07c9862ec2dd8be78aaf92293ebb12f31de437
Ibrahim Ningi <ibrahimdanladiningi3191@gmail.com>
2026-07-28 20:47:03 +0100

    Auto-generate RSA keys for Render deployment

--- utils/rsaDecrypt.js
+++ utils/rsaDecrypt.js
@@ -1,14 +1,15 @@
 const crypto = require("crypto");
 const fs = require("fs");
 
+const { privateKey } = require("./keyManager");
+
 function decryptAESKey(encryptedKeyPath) {
-  const privateKey = fs.readFileSync("./keys/private.pem", "utf8");
 
-  const encryptedKey = fs.readFileSync(encryptedKeyPath);
+    const encryptedKey = fs.readFileSync(encryptedKeyPath);
 
-  const aesKey = crypto.privateDecrypt(privateKey, encryptedKey);
+    const aesKey = crypto.privateDecrypt(privateKey, encryptedKey);
 
-  return aesKey;
+    return aesKey;
 }
 
 module.exports = decryptAESKey;
